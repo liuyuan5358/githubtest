89b83d900f1feb7d6a7d36c71170b6a4754b1887
LiuYuan <710248968@qq.com>
2016-10-17 18:35:52 +0800

    fist

--- AndroidStudioProjects/githubtest/app/src/main/java/com/example/githubtest/MainActivity.java
+++ AndroidStudioProjects/githubtest/app/src/main/java/com/example/githubtest/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        。
+        Log.i("231","41342");
         setContentView(R.layout.activity_main);
     }
 }
